f9d0dab7b0146d7aba922bb37ca5c3159ac7a3e2
صديق عبد الفتاح <  >
2017-02-07 02:34:38 +0000

    converter image & size

---
+++ IMG_Converter/Form1.Designer.vb
@@ -0,0 +1,223 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton5 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton6 = New System.Windows.Forms.RadioButton()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.LinkLabel1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LinkLabel1.Location = New System.Drawing.Point(12, 320)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.LinkLabel1.Size = New System.Drawing.Size(216, 20)
+        Me.LinkLabel1.TabIndex = 0
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "تجمع المبرمجين و المطورين العرب"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.RadioButton6)
+        Me.GroupBox2.Controls.Add(Me.RadioButton5)
+        Me.GroupBox2.Controls.Add(Me.RadioButton4)
+        Me.GroupBox2.Controls.Add(Me.RadioButton3)
+        Me.GroupBox2.Controls.Add(Me.RadioButton2)
+        Me.GroupBox2.Controls.Add(Me.RadioButton1)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 22)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(212, 128)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Size"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(249, 22)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(294, 256)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(249, 320)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(294, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "&Convert"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(283, 291)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(84, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "&Upload "
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(12, 169)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(212, 145)
+        Me.WebBrowser1.TabIndex = 6
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(425, 291)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(84, 23)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "&Clear"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(15, 19)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(54, 17)
+        Me.RadioButton1.TabIndex = 8
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "16x16"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(15, 55)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(54, 17)
+        Me.RadioButton2.TabIndex = 9
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "32x32"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(15, 93)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(54, 17)
+        Me.RadioButton3.TabIndex = 10
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "48x48"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(111, 19)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(54, 17)
+        Me.RadioButton4.TabIndex = 11
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "64x64"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'RadioButton5
+        '
+        Me.RadioButton5.AutoSize = True
+        Me.RadioButton5.Location = New System.Drawing.Point(111, 56)
+        Me.RadioButton5.Name = "RadioButton5"
+        Me.RadioButton5.Size = New System.Drawing.Size(66, 17)
+        Me.RadioButton5.TabIndex = 12
+        Me.RadioButton5.TabStop = True
+        Me.RadioButton5.Text = "100x100"
+        Me.RadioButton5.UseVisualStyleBackColor = True
+        '
+        'RadioButton6
+        '
+        Me.RadioButton6.AutoSize = True
+        Me.RadioButton6.Location = New System.Drawing.Point(111, 93)
+        Me.RadioButton6.Name = "RadioButton6"
+        Me.RadioButton6.Size = New System.Drawing.Size(66, 17)
+        Me.RadioButton6.TabIndex = 13
+        Me.RadioButton6.TabStop = True
+        Me.RadioButton6.Text = "200x200"
+        Me.RadioButton6.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(555, 356)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Name = "Form1"
+        Me.Text = "IMG Converter"
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents RadioButton6 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton5 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton4 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton3 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+
+End Class
---
+++ IMG_Converter/Form1.vb
@@ -0,0 +1,152 @@
+﻿Imports System.IO
+
+
+Public Class Form1
+    'Dim open As New OpenFileDialog
+    'Dim save As SaveFileDialog
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Dim url As String = "https://www.facebook.com/groups/devearab"
+        Process.Start(url)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+
+        WebBrowser1.DocumentText = "<MARQUEE DIRECTION=LEFT> PUT your  Ads Here</MARQUEE><br/><MARQUEE DIRECTION=right> ضع اعلانك هنا</MARQUEE>"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        OpenFileDialog1.DefaultExt = "*.BMG|*.EPS|*.GIF|*.HDR|*.EXR|*.ICO|*.JPG|*.JPEG|*.PNG|*.SVG|*.TGA|*.TIFF|*.WBMP |*.WebP"
+        OpenFileDialog1.Filter = "BMG Files|*.BMG|EPS Files|*.EPS|GIF Files|*.GIF|HDR Files|*.HDR|EXR Files|*.EXR|ICO Files|*.ICO|JPG Files|*.JPG|JPEG Files|*.JPEG|PNG Files|*.PNG|SVG Files|*.SVG|TGA Files|*.TGA|TIFF Files|*.TIFF|WBMP Files|*.WBMP|WebP Files|*.WebP"
+        'OpenFileDialog1.CreateObjRef = True
+        If OpenFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            PictureBox1.Image = System.Drawing.Bitmap.FromFile(OpenFileDialog1.FileName)
+        End If
+    End Sub
+    Public Function Convert(input_image As String, output_icon As String, size As Integer, Optional keep_aspect_ratio As Boolean = False) As Boolean
+        PictureBox1.Image.Dispose()
+        Dim input_stream As New System.IO.FileStream(input_image, System.IO.FileMode.Open)
+        Dim output_stream As New System.IO.FileStream(output_icon, System.IO.FileMode.OpenOrCreate)
+
+        Dim result As Boolean = Convert(input_stream, output_stream, size, keep_aspect_ratio)
+
+        input_stream.Close()
+        output_stream.Close()
+
+        Return result
+    End Function
+    Private Shared Function InlineAssignHelper(Of T)(ByRef target As T, value As T) As T
+        target = value
+        Return value
+    End Function
+    Public Shared Function Convert(input_stream As System.IO.Stream, output_stream As System.IO.Stream, size As Integer, Optional keep_aspect_ratio As Boolean = False) As Boolean
+        Dim input_bit As System.Drawing.Bitmap = DirectCast(System.Drawing.Bitmap.FromStream(input_stream), System.Drawing.Bitmap)
+        If input_bit IsNot Nothing Then
+            Dim width As Integer, height As Integer
+            If keep_aspect_ratio Then
+                width = size
+                height = input_bit.Height / input_bit.Width * size
+            Else
+                width = InlineAssignHelper(height, size)
+            End If
+            Dim new_bit As New System.Drawing.Bitmap(input_bit, New System.Drawing.Size(width, height))
+            If new_bit IsNot Nothing Then
+                Dim mem_data As New System.IO.MemoryStream()
+                new_bit.Save(mem_data, System.Drawing.Imaging.ImageFormat.Png)
+
+                Dim icon_writer As New System.IO.BinaryWriter(output_stream)
+                If output_stream IsNot Nothing AndAlso icon_writer IsNot Nothing Then
+
+                    icon_writer.Write(CByte(0))
+                    icon_writer.Write(CByte(0))
+
+
+                    icon_writer.Write(CShort(1))
+
+
+                    icon_writer.Write(CShort(1))
+
+                    icon_writer.Write(CByte(width))
+
+                    icon_writer.Write(CByte(height))
+
+                    icon_writer.Write(CByte(0))
+
+
+                    icon_writer.Write(CByte(0))
+
+
+                    icon_writer.Write(CShort(0))
+
+                    icon_writer.Write(CShort(32))
+
+
+                    icon_writer.Write(CInt(mem_data.Length))
+
+
+                    icon_writer.Write(CInt(6 + 16))
+
+ 
+                    icon_writer.Write(mem_data.ToArray())
+
+                    icon_writer.Flush()
+
+                    Return True
+                End If
+            End If
+            Return False
+        End If
+        Return False
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If PictureBox1.Image Is Nothing Then
+            MsgBox("please Upload samme pic ")
+        Else
+
+            Dim DESTINO As String = String.Empty
+
+            'SaveFileDialog1.DefaultExt = "*.BMG|*.EPS|*.GIF|*.HDR|*.EXR|*.ICO|*.JPG|*.JPEG|*.PNG|*.SVG|*.TGA|*.TIFF|*.WBMP |*.WebP"
+            SaveFileDialog1.Filter = "BMP Files|*.BMP|Gif Files|*.Gif|Icon Files|*.Icon|Jpeg Files|*.Jpeg|Png Files|*.Png|Tiff Files|*.Tiff" '|JPG Files|*.JPG|JPEG Files|*.JPEG|PNG Files|*.PNG|SVG Files|*.SVG|TGA Files|*.TGA|TIFF Files|*.TIFF|WBMP Files|*.WBMP|WebP Files|*.WebP
+            SaveFileDialog1.CreatePrompt = True
+            SaveFileDialog1.AddExtension = True
+            If SaveFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+                If SaveFileDialog1.FilterIndex = 1 Then
+                    PictureBox1.Image.Save(SaveFileDialog1.FileName, Imaging.ImageFormat.Bmp)
+                ElseIf SaveFileDialog1.FilterIndex = 2 Then
+                    PictureBox1.Image.Save(SaveFileDialog1.FileName, Imaging.ImageFormat.Gif)
+                ElseIf SaveFileDialog1.FilterIndex = 3 Then
+                    PictureBox1.Image.Save(SaveFileDialog1.FileName, Imaging.ImageFormat.Icon)
+                ElseIf SaveFileDialog1.FilterIndex = 4 Then
+                    PictureBox1.Image.Save(SaveFileDialog1.FileName, Imaging.ImageFormat.Jpeg)
+                ElseIf SaveFileDialog1.FilterIndex = 5 Then
+                    PictureBox1.Image.Save(SaveFileDialog1.FileName, Imaging.ImageFormat.Png)
+                ElseIf SaveFileDialog1.FilterIndex = 6 Then
+                    PictureBox1.Image.Save(SaveFileDialog1.FileName, Imaging.ImageFormat.Tiff)
+                End If
+
+            End If
+            Dim TAMAÑO As Integer = Nothing
+
+            If RadioButton1.Checked Then
+                TAMAÑO = 16
+            ElseIf RadioButton2.Checked Then
+                TAMAÑO = 32
+            ElseIf RadioButton3.Checked Then
+                TAMAÑO = 48
+            ElseIf RadioButton4.Checked Then
+                TAMAÑO = 64
+            ElseIf RadioButton5.Checked Then
+                TAMAÑO = 100
+            ElseIf RadioButton6.Checked Then
+                TAMAÑO = 200
+            End If
+           
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        PictureBox1.Image = Nothing
+    End Sub
+End Class
